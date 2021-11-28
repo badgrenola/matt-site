3b313fd8b035a65be3ed7ef227afd2a72ce0cb0a
Matt Brealey <matt@mattbrealey.com>
2021-11-28 23:53:38 +0000

    Attempt removing the SEO component

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -19,7 +19,7 @@
 </script>
 
 <script>
-  import SEO from "$lib/SEO.svelte"
+  import store from "$lib/store"
   import Header from "$lib/Header.svelte"
   import Footer from "$lib/Footer.svelte"
   import SideBar from "$lib/SideBar.svelte"
@@ -29,7 +29,26 @@
   export let projects = []
 </script>
 
-<SEO />
+<svelte:head>
+  <title>{$store.title}</title>
+  <meta name="description" content={$store.description} />
+  <meta name="keywords" content={$store.keywords}/>
+
+  <!-- Open Graph / Facebook -->
+  <meta property="og:type" content={$store.openGraph.type}>
+  <meta property="og:url" content={$store.openGraph.url}>
+  <meta property="og:title" content={$store.openGraph.title}>
+  <meta property="og:description" content={$store.openGraph.description}>
+  <meta property="og:image" content={$store.openGraph.image}>
+
+  <!-- Twitter -->
+  <meta property="twitter:card" content={$store.twitter.card}>
+  <meta property="twitter:url" content={$store.twitter.url}>
+  <meta property="twitter:title" content={$store.twitter.title}>
+  <meta property="twitter:description" content={$store.twitter.description}>
+  <meta property="twitter:image" content={$store.twitter.image}>
+</svelte:head>
+
 <div class="container mx-auto flex flex-col min-h-full max-w-5xl">
 	<Header />
 	<main class="flex-grow pb-6">
