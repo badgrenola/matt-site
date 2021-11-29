3f6c5cfa9cedddcbe2578c0b513ffc057f08cfbf
Matt Brealey <matt@mattbrealey.com>
2021-11-29 00:27:38 +0000

    Key the svelte:head content from path

--- src/lib/SEO.svelte
+++ src/lib/SEO.svelte
@@ -1,23 +1,26 @@
 <script>
   import { SEOStore } from "$lib/SEOStore";
+  import { page } from "$app/stores"
 </script>
 
 <svelte:head>
-  <title>{$SEOStore.title}</title>
-  <meta name="description" content={$SEOStore.description} />
-  <meta name="keywords" content={$SEOStore.keywords}/>
+  {#key $page.path}
+    <title>{$SEOStore.title}</title>
+    <meta name="description" content={$SEOStore.description} />
+    <meta name="keywords" content={$SEOStore.keywords}/>
 
-  <!-- Open Graph / Facebook -->
-  <meta property="og:type" content={$SEOStore.openGraph.type}>
-  <meta property="og:url" content={$SEOStore.openGraph.url}>
-  <meta property="og:title" content={$SEOStore.openGraph.title}>
-  <meta property="og:description" content={$SEOStore.openGraph.description}>
-  <meta property="og:image" content={$SEOStore.openGraph.image}>
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content={$SEOStore.openGraph.type}>
+    <meta property="og:url" content={$SEOStore.openGraph.url}>
+    <meta property="og:title" content={$SEOStore.openGraph.title}>
+    <meta property="og:description" content={$SEOStore.openGraph.description}>
+    <meta property="og:image" content={$SEOStore.openGraph.image}>
 
-  <!-- Twitter -->
-  <meta property="twitter:card" content={$SEOStore.twitter.card}>
-  <meta property="twitter:url" content={$SEOStore.twitter.url}>
-  <meta property="twitter:title" content={$SEOStore.twitter.title}>
-  <meta property="twitter:description" content={$SEOStore.twitter.description}>
-  <meta property="twitter:image" content={$SEOStore.twitter.image}>
+    <!-- Twitter -->
+    <meta property="twitter:card" content={$SEOStore.twitter.card}>
+    <meta property="twitter:url" content={$SEOStore.twitter.url}>
+    <meta property="twitter:title" content={$SEOStore.twitter.title}>
+    <meta property="twitter:description" content={$SEOStore.twitter.description}>
+    <meta property="twitter:image" content={$SEOStore.twitter.image}>
+  {/key}
 </svelte:head>
